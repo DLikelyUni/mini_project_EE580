10548aae0c90d2b909a42615e859391020d5ad6e
Duncan Likely (Student) <duncan.likely.2021@uni.strath.ac.uk>
2026-03-12 19:50:50 +0000

    added PRD audio read and semaphor put

--- CCS/mini_proj/hello.c
+++ CCS/mini_proj/hello.c
@@ -19,13 +19,69 @@
 
 #include "hellocfg.h"
 
+#include "framework.h"
+
+#define BIT0 0x01
+#define BIT1 0x02
+#define BIT2 0x04
+#define BIT3 0x08
+#define BIT4 0x10
+#define BIT5 0x20
+#define BIT6 0x40
+#define BIT7 0x80
+
+#define BUFFLEN 64
+
+
+int16_t s16;
+float sampBuffer[BUFFLEN] = {0};
+uint16_t buff_idx_ptr = 0;
+
 /*
  *  ======== main ========
  */
-Void main()
+void main(void)
 {
     LOG_printf(&trace, "hello world!");
+
+    //BINTEN
+    initAll();
 
     /* fall into DSP/BIOS idle loop */
     return;
 }
+
+void dipPRD(void){
+
+    uint32_t dip_status;
+    DIP_getAll(&dip_status);
+    uint32_t mask =  (BIT0 | BIT1 | BIT5 | BIT6 | BIT7);
+
+    if (dip_status == mask){
+        LED_toggle(LED_1);
+        LED_turnOff(LED_2);
+    } else {
+        LED_toggle(LED_2);
+        LED_turnOff(LED_1);
+    }
+
+
+}
+
+void processSamp(void){
+
+    do {
+        SEM_pend(&sem, SYS_FOREVER);
+        uint16_t sample = s16;
+        sampBuffer[buff_idx_ptr] = sample;
+        buff_idx_ptr = (buff_idx_ptr++ & BUFFLEN);
+    } while(1);
+
+}
+
+void audioHWI(void){
+
+    s16 = read_audio_sample();
+    SEM_post(&sem);
+}
+
