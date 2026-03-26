0fb7afb872c0228f997ff5d1b1607169b6ddf8e4
Duncan Likely (Student) <duncan.likely.2021@uni.strath.ac.uk>
2026-03-26 21:32:46 +0000

    updated audio read

--- CCS/mini_proj/hello.c
+++ CCS/mini_proj/hello.c
@@ -32,9 +32,9 @@
 #define BIT6 0x40
 #define BIT7 0x80
 
-#define BUFFLEN 32768
-#define IIR_A_LEN 64
-#define IIR_B_LEN 64
+#define BUFFLEN  0x8000
+#define IIR_A_LEN 16
+#define IIR_B_LEN 16
 
 #define REC_LOOPBACK BIT0
 #define PLAYBACK_BUFF (BIT0 | BIT1)
@@ -50,19 +50,21 @@
 
 
 int16_t sampBuffer[BUFFLEN] = {0};
-uint16_t buff_idx_ptr = 0;
+volatile uint32_t buff_idx_ptr = 0;
 
 int16_t iir_x_buff[IIR_A_LEN] = {0};
 
 float lp_y_buff[IIR_B_LEN] = {0};
-float bp_y_buff[IIR_B_LEN] = {0};
+//float bp_y_buff[IIR_B_LEN] = {0};
 float hp_y_buff[IIR_B_LEN] = {0};
 
 uint8_t iir_idx_ptr = 0;
 
+uint32_t dip_status;
+volatile uint32_t playback_mode;
+volatile uint32_t filter_sel;
 volatile int16_t s16;
-volatile int16_t y16 = 0;
-
+volatile int16_t y16;
 /*
  *  ======== main ========
  */
@@ -72,6 +74,17 @@ void main(void)
 
     //BINTEN
     initAll();
+    int i;
+    for (i=0; i < 15; i++){
+        a_lp[i] = a_lp[i] / 2;
+        b_lp[i] = b_lp[i] / 2;
+        a_hp[i] = a_hp[i] / 2;
+        b_hp[i] = b_hp[i] / 2;
+    }
+    for (i=0; i < 31; i++){
+            a_bp[i] = a_bp[i] / 2;
+            b_bp[i] = b_bp[i] / 2;
+    }
 
     /* fall into DSP/BIOS idle loop */
     return;
@@ -79,7 +92,6 @@ void main(void)
 
 void dipPRD(void){
 
-    uint32_t dip_status;
     DIP_getAll(&dip_status);
     uint32_t mask =  (BIT0 | BIT1 | BIT5 | BIT6 | BIT7);
 
@@ -90,6 +102,10 @@ void dipPRD(void){
         LED_toggle(LED_2);
         LED_turnOff(LED_1);
     }
+        //uint32_t mask = (BIT0 | BIT1);
+
+    playback_mode = dip_status & MASK_PLAYBACK;
+    filter_sel = dip_status & MASK_FILT;
 
 
 }
@@ -98,6 +114,7 @@ static inline int16_t iir_filt(int16_t *iir_x_buff, float *iir_y_buff, uint8_t i
     int i;
     uint8_t idx = iir_idx_ptr;
     float sum = 0;
+#pragma UNROLL(16)
     for(i = 0; i < filt_len; i++){
         sum += (float)iir_x_buff[idx]*iir_a_coef[i];
         sum += iir_y_buff[idx]*iir_b_coef[i];
@@ -112,69 +129,81 @@ void processSamp(void){
 
 
 
-    uint32_t dip_status;
-    DIP_getAll(&dip_status);
-    //uint32_t mask = (BIT0 | BIT1);
-
-    uint32_t playback_mode = dip_status & MASK_PLAYBACK;
-    uint32_t filter_sel = dip_status & MASK_FILT;
-
-    /*float y_lp = 0;
-    float y_bp = 0;
-    float y_hp = 0;*/
-
-    switch (playback_mode){
-    case REC_LOOPBACK:
-        sampBuffer[buff_idx_ptr] = s16;
-        y16 = s16;
-        break;
-    case PLAYBACK_BUFF:
-        //y16 = sampBuffer[buff_idx_ptr];
-        iir_x_buff[iir_idx_ptr] = sampBuffer[buff_idx_ptr];
-
-        lp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, lp_y_buff, iir_idx_ptr, a_lp, b_lp, N_LOWPASS_B);
-        bp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, bp_y_buff, iir_idx_ptr, a_bp, b_bp, N_BANDPASS_B);
-        hp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, hp_y_buff, iir_idx_ptr, a_hp, b_hp, N_HIGHPASS_B);
-
-        switch (filter_sel){
-        case LP_OUT:
-            y16 = (int16_t)lp_y_buff[iir_idx_ptr];
-            break;
-        case BP_OUT:
-            y16 = (int16_t)bp_y_buff[iir_idx_ptr];
-            break;
-        case LP_BP_OUT:
-            y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + bp_y_buff[iir_idx_ptr]);
-            break;
-        case HP_OUT:
-            y16 = (int16_t)hp_y_buff[iir_idx_ptr];
-            break;
-        case LP_HP_OUT:
-            y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + hp_y_buff[iir_idx_ptr]);
-            break;
-        case LP_BP_HP_OUT:
-            y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + bp_y_buff[iir_idx_ptr] + hp_y_buff[iir_idx_ptr]);
-            break;
-        }
-
-        iir_idx_ptr = (iir_idx_ptr++ & (IIR_B_LEN-1));
-        break;
-    default:
-        y16 = 0;
-        break;
-    }
-    buff_idx_ptr = (buff_idx_ptr++ & (BUFFLEN-1));
-
-
-
 }
 
 void audioHWI(void){
 
     //volatile int16_t s16;
+    //volatile int16_t s16;
+    //volatile int16_t y16 = 0;
+
     s16 = read_audio_sample();
+
+
+
+
+    /*float y_lp = 0;
+    float y_bp = 0;
+    float y_hp = 0;*/
+
+
+
+    if(MCASP->RSLOT){
+        //SWI_post(&SWI0);
+        switch (playback_mode){
+                case REC_LOOPBACK:
+                    sampBuffer[buff_idx_ptr] = s16;
+                    y16 = s16;
+                    break;
+                case PLAYBACK_BUFF:
+                    //y16 = sampBuffer[buff_idx_ptr];
+                    iir_x_buff[iir_idx_ptr] = sampBuffer[buff_idx_ptr];
+
+                    //lp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, lp_y_buff, iir_idx_ptr, a_lp, b_lp, N_LOWPASS_B);
+                    //bp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, bp_y_buff, iir_idx_ptr, a_bp, b_bp, N_BANDPASS_B);
+                    hp_y_buff[iir_idx_ptr] = iir_filt(iir_x_buff, hp_y_buff, iir_idx_ptr, a_hp, b_hp, N_HIGHPASS_B);
+
+                    switch (filter_sel){
+                    case LP_OUT:
+                        y16 = (int16_t)lp_y_buff[iir_idx_ptr];
+                        break;
+                    /*case BP_OUT:
+                        y16 = (int16_t)bp_y_buff[iir_idx_ptr];
+                        break;
+                    case LP_BP_OUT:
+                        y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + bp_y_buff[iir_idx_ptr]);
+                        break;*/
+                    case HP_OUT:
+                        y16 = (int16_t)hp_y_buff[iir_idx_ptr];
+                        break;
+                    case LP_HP_OUT:
+                        y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + hp_y_buff[iir_idx_ptr]);
+                        break;
+                    /*case LP_BP_HP_OUT:
+                        y16 = (int16_t)(lp_y_buff[iir_idx_ptr] + bp_y_buff[iir_idx_ptr] + hp_y_buff[iir_idx_ptr]);
+                        break;*/
+                    default:
+                        y16 = sampBuffer[buff_idx_ptr];
+
+                        break;
+                    }
+
+                    iir_idx_ptr++;
+                    iir_idx_ptr &= (IIR_B_LEN-1);
+                    break;
+                default:
+                    y16 = 0;
+                    break;
+                }
+                buff_idx_ptr++;
+
+                buff_idx_ptr &= (BUFFLEN-1);
+    } else {
+        y16 = 0;
+    }
+
+
     write_audio_sample(y16);
-    SWI_post(&SWI0);
 
 }
 
